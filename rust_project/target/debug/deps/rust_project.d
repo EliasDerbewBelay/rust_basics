25942020c022602003a5473bddc3184993f3ea4e
eliasderbewbelay <eliasderbew1@gmail.com>
2026-02-22 00:23:48 +0300

    initial commit

---
+++ rust_project/target/debug/deps/rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\elias\rust_basics\rust_project\target\debug\deps\rust_project.d: src\main.rs
+
+C:\Users\elias\rust_basics\rust_project\target\debug\deps\rust_project.exe: src\main.rs
+
+src\main.rs:
---
+++ rust_project/target/debug/rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\elias\rust_basics\rust_project\target\debug\rust_project.exe: C:\Users\elias\rust_basics\rust_project\src\main.rs
